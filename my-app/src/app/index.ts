8f2fe3d139e4ef914d2b927b763665f9ec32071b
bsucs <122378640+bsucs@users.noreply.github.com>
2024-09-30 22:53:09 -0400

    changed

--- my-app/src/app/index.ts
+++
@@ -1,8 +0,0 @@
-import store from './app/store'
-import { Provider } from 'react-redux'
-ReactDOM.render(
-<Provider store={store}>
-<App />
-</Provider>,
-document.getElementById('root')
-)
---
+++ my-app/src/app/pages.tsx
@@ -0,0 +1,17 @@
+// src/index.tsx (or App.tsx)
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import App from '../App';
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+
+  
+
+);
